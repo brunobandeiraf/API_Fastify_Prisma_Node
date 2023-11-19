cbc6b173d7182b549f7b61c69f4a20c031438250
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-11-19 17:50:35 -0300

    controller users/register

---
+++ src/http/controllers/register.ts
@@ -0,0 +1,21 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { prisma } from '@/lib/prisma'
+import { z } from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+  })
+
+  const { name, email } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email
+    },
+  })
+
+  return reply.status(201).send()
+}
